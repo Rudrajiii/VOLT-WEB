e27519459be95a36f962d27f40104316dcf7c83f
rudrajiii <rudrasaha305@gmail.com>
2025-03-29 18:02:24 +0530

    buttons changed

--- client/volty/src/components/right-section/RightSection.jsx
+++ client/volty/src/components/right-section/RightSection.jsx
@@ -7,6 +7,7 @@ import { SiApplemusic } from "react-icons/si";
 import { MdManageAccounts } from "react-icons/md";
 import { FaCodeBranch } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 const RightSection = () => {
   return (
@@ -62,7 +63,7 @@ const RightSection = () => {
           {/* Bot Status */}
           <div className="mt-4 flex justify-center gap-4 text-sm text-gray-400">
             {/* Uptime Block */}
-            <div className="bg-[#29292D] px-4 py-2 rounded-lg text-white shadow-sm flex items-center">
+            <div className="bg-[#29292D] cursor-pointer font-medium px-4 py-2 rounded-lg text-white shadow-sm flex items-center">
               <svg
                 aria-hidden="true"
                 focusable="false"
@@ -78,30 +79,17 @@ const RightSection = () => {
                   d="M64 32C28.7 32 0 60.7 0 96l0 64c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-64c0-35.3-28.7-64-64-64L64 32zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm48 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zM64 288c-35.3 0-64 28.7-64 64l0 64c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-64c0-35.3-28.7-64-64-64L64 288zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm56 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"
                 />
               </svg>
-              <span className="text-green-400 font-medium mr-1">Uptime </span> 3 days 5 hours
+              {/* <span className="text-green-400 font-medium mr-1">Uptime </span> 3 days 5 hours */}
+              <span className="text-green-400 font-medium mr-1"></span> Join Support Server
             </div>
 
             {/* Ping Block */}
-            <div className="bg-[#29292D] px-6 py-3 rounded-lg shadow-md flex items-center w-40">
-              <span className="text-green-400 font-medium ml-1 mr-1 flex items-center text-sm">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="wifi"
-                  className="svg-inline--fa fa-wifi text-green-500 mr-1 w-4 h-4"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 640 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"
-                  ></path>
-                </svg>
-                Ping
+            <div className="bg-[#29292D] px-6 py-3 cursor-pointer rounded-lg shadow-md flex items-center w-48">
+              <span className="text-white font-medium ml-1 mr-1 gap-2 flex items-center text-sm">
+              <FaStar className='text-green-500 text-xl'/>
+                Vote on top.gg
               </span>
-              <span className="text-white ml-1 text-sm">45ms</span>
+              {/* <span className="text-white ml-1 text-sm">45ms</span> */}
             </div>
           </div>
         </div>
